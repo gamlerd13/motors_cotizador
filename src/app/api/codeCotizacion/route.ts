6fb04e1f96163dc9b1c2b2c5a9aa6356332e879d
gamlerd13 <gamlerd13@gmail.com>
2024-07-30 09:30:33 -0500

    feat: change code logic

--- src/app/api/codeCotizacion/route.ts
+++ src/app/api/codeCotizacion/route.ts
@@ -3,14 +3,10 @@ import prisma from "@/libs/db";
 
 export async function GET() {
   try {
-    const lastCode = await prisma.codeCotizacion.findFirst({
-      orderBy: {
-        id: "desc",
-      },
-    });
-    let newNumber = 1;
     const prefix = "2024-";
-    if (lastCode) newNumber = lastCode.nextCode;
+    let newNumber = 1;
+    const lastCode = await prisma.codeCotizacion.findFirst();
+    if (lastCode) newNumber = lastCode.nextCode + 1;
     const code = `${prefix}${newNumber.toString().padStart(4, "0")}`;
 
     return NextResponse.json(code, { status: 200 });
